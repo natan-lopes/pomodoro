47fb21786849172e17beb990b4997380761cfefe
Natan Lopes da Silveira <131193061+natan-lopes@users.noreply.github.com>
2024-03-09 11:55:33 -0300

    html-css-ok

---
+++ script.js
@@ -0,0 +1,8 @@
+let acao = document.getElementById('acao');
+let pausa = document.getElementById('pausa');
+let sessoes = document.getElementById('sessoes');
+let segundos;
+
+var bell = new Audio("./audio/bell.mp3");
+var volta = new Audio("./audio/volta.mp3");
+var final = new Audio("./audio/final.mp3");
